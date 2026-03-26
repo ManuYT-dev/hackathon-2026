3bf79127dab0d1c7e79958b3c2a7c0e37fa48f39
ITzZysn <macsaipeti2010@gmail.com>
2026-03-26 11:14:26 +0100

    Update MainWindowMap.xaml.cs

--- MainWindowMap.xaml.cs
+++ MainWindowMap.xaml.cs
@@ -32,37 +32,25 @@ namespace Hackathon
         {
             InitializeComponent();
 
-            
-            
-
-
             LoggingWidget.ShowLoggingInMap = ActiveMode.No;
+            mapControl.Map.Widgets.Clear();
+
             Map map = new Map();
-            // OSM-Tile-Layer
             map.Layers.Add(OpenStreetMap.CreateTileLayer());
 
-            map.Layers.Add(OpenStreetMap.CreateTileLayer());
-
-            // Map dem Control zuweisen
             mapControl.Map = map;
-
-            // Wien Koordinaten
             var position = SphericalMercator.FromLonLat(9.7415, 47.4125);
-
             mapControl.Map.Navigator.OverrideZoomBounds = new MMinMax(10, 1000);
-            // 👉 WICHTIG: Navigator benutzen (ohne Resolutions!)
             mapControl.Map.Navigator.CenterOn(position.x, position.y);
-
-            // Zoom setzen (z. B. 1000 = näher, 10000000 = weiter raus)
             mapControl.Map.Navigator.ZoomTo(10);
 
             var lon = 9.7415; var lat = 47.4125;
             var point = SphericalMercator.FromLonLat(lon, lat);
             var feature = new PointFeature(point);
-            feature.Styles.Add(new SymbolStyle { SymbolType = SymbolType.Ellipse, Fill = new Mapsui.Styles.Brush(new Mapsui.Styles.Color(255, 0, 0)), SymbolScale = 0.2, Outline = new Mapsui.Styles.Pen { Color = new Mapsui.Styles.Color(255, 255, 255, 0), Width = 0 } });
+            feature.Styles.Add(new SymbolStyle { SymbolType = SymbolType.Ellipse, Fill = new Mapsui.Styles.Brush(new Mapsui.Styles.Color(255, 0, 0)), SymbolScale = 0.3, Outline = new Mapsui.Styles.Pen { Color = new Mapsui.Styles.Color(255, 255, 255, 0), Width = 0 } });
             var layer = new MemoryLayer { Features = new[] { feature }, Style=null };
             mapControl.Map.Layers.Add(layer);
-
+           
 
         }
     }
