542e46c36e1ce735326eb4e3486639656fdc4103
K D <karim.debbabi4@gmail.com>
2026-03-26 16:13:56 +0100

    Create Class1.cs

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Hackathon
+{
+    internal class Class1
+    {
+        public static async Task<(double lat, double lon)> GetCoords()
+        {
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0");
+
+            string html = await client.GetStringAsync("https://ipgeolocation.io/what-is-my-ip");
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var codeBlock = htmlDoc.DocumentNode.SelectSingleNode("//code");
+
+            string jsonText = System.Net.WebUtility.HtmlDecode(codeBlock.InnerText);
+
+            using var data = JsonDocument.Parse(jsonText);
+            var location = data.RootElement.GetProperty("location");
+
+            double lat = double.Parse(location.GetProperty("latitude").GetString(),
+                                      System.Globalization.CultureInfo.InvariantCulture);
+            double lon = double.Parse(location.GetProperty("longitude").GetString(),
+                                      System.Globalization.CultureInfo.InvariantCulture);
+
+            return (lat, lon);
+        }
+    }
+}
