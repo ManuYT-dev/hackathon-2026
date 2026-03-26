bfd9ac37d331631da10807851698891f55d10cb2
csharp-maxxer <valentin.haensler@posteo.de>
2026-03-26 12:25:22 +0100

    commit

---
+++ MicrobitController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Hackathon
+{
+    public class MicrobitController
+    {
+        private SerialPort _port;
+
+        public bool Connect(string portName = "COM16")
+        {
+            try
+            {
+                _port = new SerialPort(portName, 115200);
+                _port.Open();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public void SendAngle(double angle)
+        {
+            if (_port?.IsOpen == true)
+                _port.WriteLine(((int)angle).ToString());
+        }
+
+        public void Disconnect() => _port?.Close();
+    }
+}
